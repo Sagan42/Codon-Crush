91c9782c2be5e4506c8f72bc0567e730e4dc3dc0
Sagan42 <calebejpcf@gmail.com>
2018-10-02 01:43:46 -0300

    Grade de bases implementada

---
+++ Assets/Script/GameGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour {
+    public int xSize, ySize;
+    private GameObject[] bases;
+    public float basesEspace = 1;
+    private Grid[,] item;
+
+    void Start()
+    {
+        Getbases();
+        FillGrid();
+    }
+
+    void FillGrid()
+    {
+        item = new Grid[xSize, ySize];
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                item [x, y] = Instance(x, y);
+            }
+        }
+    }
+
+    Grid Instance (int x, int y)
+    {
+        GameObject randombase = bases[Random.Range(0, bases.Length)];
+        GameObject newbase = (GameObject) Instantiate(randombase, new Vector3(x * basesEspace, y), Quaternion.identity);
+        newbase.GetComponent<Grid>().ChangePosition(x, y);
+        return newbase.GetComponent<Grid>();
+    }
+
+    void Getbases()
+    {
+        bases = Resources.LoadAll <GameObject> ("Prefabs");
+        for (int i = 0; i < bases.Length; i++)
+        {
+            bases[i].GetComponent<Grid>().id = i;
+        }
+    }
+}
---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public int x
+    {
+        get;
+        private set;
+    }
+
+    public int y
+    {
+        get;
+        private set;
+    }
+
+    [HideInInspector] public int id;
+
+    public void ChangePosition (int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+        gameObject.name = string.Format("Sprite [{0}] [{1}]", x, y);
+    }
+
+    private void OnMouseDown()
+    {
+        if (OnMouseOverItemEvent != null)
+        {
+            OnMouseOverItemEvent(this);
+        }
+    }
+
+    public delegate void OnMouseOverItem(Grid item);
+    public static event OnMouseOverItem OnMouseOverItemEvent;
+}
